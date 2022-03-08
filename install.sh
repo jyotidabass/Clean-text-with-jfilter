0d65a052647c364d83b7ba0d0831a5ef18e68f11
Jyoti Dabass <34393429+jyotidabass@users.noreply.github.com>
2022-03-08 14:55:42 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+unset PYTORCH_VERSION
+# For unittest, nightly PyTorch is used as the following section,
+# so no need to set PYTORCH_VERSION.
+# In fact, keeping PYTORCH_VERSION forces us to hardcode PyTorch version in config.
+
+set -e
+
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+root_dir="$(git rev-parse --show-toplevel)"
+
+cd "${root_dir}"
+
+eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
+conda activate ./env
+
+printf "* Installing PyTorch\n"
+conda install -y -c "pytorch-${UPLOAD_CHANNEL}" ${CONDA_CHANNEL_FLAGS} pytorch cpuonly
+
+printf "Installing torchdata from source\n"
+pip install git+https://github.com/pytorch/data.git
+
+printf "* Installing torchtext\n"
+git submodule update --init --recursive
+"$root_dir/packaging/vc_env_helper.bat" python setup.py develop
+
+printf "* Installing parameterized\n"
+pip install parameterized
